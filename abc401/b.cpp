c21677d2eb022521d282569d677b46a645108843
tossy0423 <syun111@gmail.com>
2025-04-12 22:42:21 +0900

    入力のところで失敗

---
+++ abc401/b.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include <string>
+#include <vector>
+#include <iostream>
+int main()
+{
+    int n;
+    std::cin >> n;
+    std::vector<string> s;
+    // for(int i = 0; i < n; i++)std::cin >> s.at(i);
+
+    // std::cout << "------" << std::endl;
+    // std::cout << n << std::endl;
+    // for(int i = 0; i < n; i++)std::cout << s[n] << std::endl;
+
+
+    return 0;
+}
