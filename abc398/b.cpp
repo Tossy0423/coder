29b82748c8a89bbe01152d0ce87f3729cfcb2701
tossy0423 <syun111@gmail.com>
2025-04-14 21:49:06 +0900

    :+1: 解説読んで理解できた

--- abc398/b.cpp
+++ abc398/b.cpp
@@ -2,21 +2,18 @@
 int main()
 {
     std::vector<int> a(7);
+
+    // 入力
     for(int i = 0; i < 7; i++)std::cin >> a.at(i);
-    // for(int i = 0; i < 7; i++)std::cout << a[i] << " " << std::endl;
 
-    int record[14];
+    // カウント用配列定義して入力された数をカウント
+    std::vector<int> record(14,0);
     for(int i = 0; i < 7; i++)record[a[i]]++;
-    // for(int i = 0; i < 14; i++)std::cout << record[i] << ", ";
 
-    bool flag = false;
-    for(int i = 0; i < 14; i++)
-    {
-        if(record[i] == 3)for(int j=0; j < 14; j++)if(record[j] == 2)flag = true;
-        else if(record[i] == 2)for(int j=0; j < 14; j++)if(record[j] == 3)flag = true;
-    }
+    // ソート
+    std::sort(record.rbegin(), record.rend());
 
-    if(flag)std::cout << "Yes";
+    if(3 <= record[0] && 2 <= record[1])std::cout << "Yes";
     else std::cout << "No";
     
     return 0;
