cf67707b025fe288ee4dbf3a403050cd22476685
tossy0423 <syun111@gmail.com>
2025-04-14 20:59:31 +0900

    :pencil: did it

--- abc398/a.cpp
+++ abc398/a.cpp
@@ -1,5 +1,26 @@
 #include<iostream>
 int main()
 {
+    int n;
+    std::cin >> n;
+
+    if(n % 2 == 0)
+    {
+        for(int i = 0; i < n; i++)
+        {
+            if(i < (n-2)/2)std::cout << "-";
+            else if ((n-2)/2 <= i && i <= ((n-2)/2 +1)) std::cout << "=";
+            else if ((n-2)/2 < i && i <= n) std::cout << "-";
+        }
+    }else
+    {
+        for(int i = 0; i < n; i++)
+        {
+            if(i < (n-1)/2)std::cout << "-";
+            else if ((n-1)/2 <= i && i <= ((n-1)/2)) std::cout << "=";
+            else if ((n-1)/2 < i && i <= n) std::cout << "-";
+        }
+    }
+
     return 0;
 }
