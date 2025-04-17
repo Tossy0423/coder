ae7426bd1532b97c5cd0fd7dfd88f75f73b1093c
tossy0423 <syun111@gmail.com>
2025-04-17 23:49:40 +0900

    :pencil: test pass

--- abc397/a.cpp
+++ abc397/a.cpp
@@ -1,5 +1,12 @@
 #include <bits/stdc++.h>
 int main()
 {
+    float x;
+    std::cin >> x;
+
+    if(x < 37.5)std::cout << "3";
+    else if(37.5 <= x && x < 38.0)std::cout << "2";
+    else if(38.0 <= x)std::cout << "1";
+
     return 0;
 }
