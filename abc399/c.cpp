a347776d570587fae0ca615eb7ae6adafed69917
tossy0423 <syun111@gmail.com>
2025-04-18 21:49:32 +0900

    :bug: 写経しただけ, 理解できていないので見直す

--- abc399/c.cpp
+++ abc399/c.cpp
@@ -1,10 +1,30 @@
 #include <bits/stdc++.h>
+#include <atcoder/all>
+
 int main()
 {
     int n, m;
     std::cin >> n >> m;
-    std::map<int, int> u;
-    for(int i = 0; i < n; i++)std::cin >> u[i][i];
+
+    int ans = 0;
+    atcoder::dsu uf(n);
+
+    for(int i = 0; i < n; i++)
+    {
+        int a, b;
+        std::cin >> a >> b;
+        a -= 1; 
+        b -= 1;
+        
+        if(uf.same(a, b))
+        {
+            ans++;
+            continue;
+        }
+        uf.merge(a, b);
+    }
+
+    std::cout << ans << std::endl;
 
     return 0;
 }
